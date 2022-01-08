dc4aba5bb4dda4c2af61721156d7ce71c1dec126
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-08 11:41:49 +0530

    Created Post entity

--- src/main/java/io/yadnyesh/springbootblog/entity/Post.java
+++ src/main/java/io/yadnyesh/springbootblog/entity/Post.java
@@ -1,13 +1,42 @@
 package io.yadnyesh.springbootblog.entity;
 
 import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "posts",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})
+        })
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "title", nullable = false)
     private String title;
+    @Column(name = "description")
     private String description;
+    @Column(name = "content")
     private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Post post = (Post) o;
+        return id != null && Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
