5c51bd4b568bebf29256ee90eab56957963399f7
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-08 12:25:28 +0530

    GetAllPosts

--- src/main/java/io/yadnyesh/springbootblog/service/PostService.java
+++ src/main/java/io/yadnyesh/springbootblog/service/PostService.java
@@ -2,6 +2,9 @@ package io.yadnyesh.springbootblog.service;
 
 import io.yadnyesh.springbootblog.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+    List<PostDto> getAllPosts();
 }
