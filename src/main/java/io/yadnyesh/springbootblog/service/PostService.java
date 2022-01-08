01641caadd6ce1b843216d6635cab85f1ac93876
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-08 19:26:28 +0530

    Sorting implemented successfully

--- src/main/java/io/yadnyesh/springbootblog/service/PostService.java
+++ src/main/java/io/yadnyesh/springbootblog/service/PostService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
-    PostResponse getAllPosts(int pageNo, int pageSize);
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
     PostDto getPostById(Long postId);
     PostDto updatePost(PostDto postDto, Long postId);
     void deletePostId(Long postId);
