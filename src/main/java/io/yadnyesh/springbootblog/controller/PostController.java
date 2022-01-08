875cc3edafe19875662ee72f53732a480d1baa97
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-08 12:10:01 +0530

    Created Post Controller

---
+++ src/main/java/io/yadnyesh/springbootblog/controller/PostController.java
@@ -0,0 +1,23 @@
+package io.yadnyesh.springbootblog.controller;
+
+import io.yadnyesh.springbootblog.payload.PostDto;
+import io.yadnyesh.springbootblog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
