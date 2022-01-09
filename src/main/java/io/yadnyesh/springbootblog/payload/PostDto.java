225d88eecc74f54ac1577f0e5591c77a67a715fb
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-09 15:29:46 +0530

    Validation and Validation Response

--- src/main/java/io/yadnyesh/springbootblog/payload/PostDto.java
+++ src/main/java/io/yadnyesh/springbootblog/payload/PostDto.java
@@ -2,6 +2,8 @@ package io.yadnyesh.springbootblog.payload;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 
@@ -13,8 +15,14 @@ import java.util.Set;
 @EqualsAndHashCode
 public class PostDto {
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Title Should have at least 2 characters")
     private String title;
+    @NotEmpty
+    @Size(min = 10, message = "Title Should have at least 10 characters")
     private String description;
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
